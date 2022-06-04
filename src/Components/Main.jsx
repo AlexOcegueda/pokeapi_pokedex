d442b9081d9f0650797d7d0b8a2059aadde998ef
AlexOcegueda <alexocegueda59@gmail.com>
2022-06-04 18:47:37 -0500

    adjusted abilities section look

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Card from "./Card";
+import Pokeinfo from "./Pokeinfo";
+
 const Main=()=>{
     return (
         <>
@@ -13,7 +15,7 @@ const Main=()=>{
                     <Card/>
                 </div>
                 <div className="right-content">
-
+                    <Pokeinfo/>
                 </div>
             </div>
         </>
